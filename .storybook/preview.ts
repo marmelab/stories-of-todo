58a6d6dec7fdaa719d457ddd8da75b63604bb476
adrien guernier <adrien@marmelab.com>
2024-08-01 17:29:27 +0200

    customize storybook theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,12 @@
 import type { Preview } from "@storybook/react";
 import "../src/App.css";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
