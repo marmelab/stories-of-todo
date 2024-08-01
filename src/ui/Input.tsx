1dc2ae4b72b766958f1d361791eb0250d9b49c20
adrien guernier <adrien@marmelab.com>
2024-08-01 14:38:24 +0200

    make input required

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -5,12 +5,14 @@ export const Input = <T extends FieldValues>(props: InputProps<T>) => {
   const { register } = useFormContext();
   const { name, ...rest } = props;
   return (
-    <input
-      type="text"
-      placeholder="What do you want to do?"
-      className="input input-bordered input-lg w-full max-w-xs join-item"
-      {...register(name)}
-      {...rest}
-    />
+    <>
+      <input
+        type="text"
+        placeholder="What do you want to do?"
+        className="input input-bordered input-lg w-full max-w-xs join-item"
+        {...(register(name), { required: true })}
+        {...rest}
+      />
+    </>
   );
 };
