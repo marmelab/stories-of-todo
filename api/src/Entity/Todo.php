b82a9f068fa795c80ccf0261c11a530e2dada133
adrien guernier <adrien@marmelab.com>
2024-08-21 22:03:28 +0200

    add api backend for test

---
+++ api/src/Entity/Todo.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+enum Priority: string
+{
+    case low = 'low';
+    case medium = 'medium';
+    case high = 'high';
+}
+
+#[ORM\Entity(repositoryClass: TodoRepository::class)]
+#[ApiResource]
+class Todo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?bool $completed = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dueDate = null;
+
+    #[ORM\Column(length: 255, enumType: Priority::class)]
+    private ?Priority $priority = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function isCompleted(): ?bool
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(bool $completed): static
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTimeImmutable
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTimeImmutable $dueDate): static
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getPriority(): ?Priority
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(Priority $priority): static
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+}
