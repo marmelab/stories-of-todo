58ac343c9cd02365d447f24c2969ce1c34db5694
adrien guernier <adrien@marmelab.com>
2024-09-14 20:24:14 +0200

    chore: update hero

--- src/slides/Hero.tsx
+++ src/slides/Hero.tsx
@@ -1,14 +1,18 @@
 import { SlideLayout } from "spectacle";
 import me from "@/assets/me.jpg";
+import background from "@/assets/background-0.jpg";
 
 export const Hero = () => (
   <SlideLayout.VerticalImage
+    backgroundImage={`url(${background})`}
     src={me}
     alt="Adrien Guernier"
     title="Adrien Guernier"
+    imgContainerProps={{ style: { borderRadius: "100%" } }}
     listItems={[
       "Développeur fullstack chez Marmelab",
       "Membre de la Core Team de React-Admin",
+      "PHP anthousiast",
     ]}
-  ></SlideLayout.VerticalImage>
+  />
 );
