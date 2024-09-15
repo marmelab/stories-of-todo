6dba5a6ccd3a30e94408049a9a8bbcc4f93b1984
adrien guernier <adrien@marmelab.com>
2024-09-15 13:12:10 +0200

    add link to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,34 @@
 import { NavLink } from "react-router-dom";
 
 export const Home = () => (
-  <div className="container mx-auto items-center text-center">
-    <h1 className="text-3xl font-bold text-center mt-8 mb-8">
-      Stories Of Todo
-    </h1>
-    <div className="flex flex-col gap-4">
-      <NavLink className="btn btn-outline btn-lg btn-accent" to="todos">
-        Todos
-      </NavLink>
-      <NavLink className="btn btn-outline btn-lg btn-info" to="slideshow">
-        Slideshow
-      </NavLink>
-      <NavLink className="btn btn-outline btn-lg btn-warning" to="admin">
-        Admin
-      </NavLink>
+  <div className="items-center justify-center h-lvh flex">
+    <div>
+      <h1 className="text-4xl font-bold mt-8 mb-8">Stories Of Todo</h1>
+      <div className="flex flex-col gap-4">
+        <NavLink className="btn btn-outline btn-lg btn-info" to="slideshow">
+          Slideshow
+        </NavLink>
+        <NavLink
+          className="btn btn-outline btn-lg btn-error"
+          to="http://localhost:6006"
+          target="_blank"
+        >
+          Storybook
+        </NavLink>
+        <NavLink
+          className="btn btn-outline btn-lg btn-success"
+          to="https://localhost"
+          target="_blank"
+        >
+          API
+        </NavLink>
+        <NavLink className="btn btn-outline btn-lg btn-accent" to="todos">
+          Todos
+        </NavLink>
+        <NavLink className="btn btn-outline btn-lg btn-warning" to="admin">
+          Admin
+        </NavLink>
+      </div>
     </div>
   </div>
 );
