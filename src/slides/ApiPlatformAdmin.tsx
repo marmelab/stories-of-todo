54de2582dbbe4865ce7819bf2d8c627a01c3a826
adrien guernier <adrien@marmelab.com>
2024-09-16 19:42:09 +0200

    chore: remove old gif

--- src/slides/ApiPlatformAdmin.tsx
+++ src/slides/ApiPlatformAdmin.tsx
@@ -1,12 +1,9 @@
-import { Slide, Notes } from "spectacle";
-import adminDemo from "@/assets/admin-demo.gif";
+import { Slide, Notes, Heading } from "spectacle";
 
 export const ApiPlatformAdmin = () => (
-  <Slide
-    backgroundImage={`url(${adminDemo})`}
-    backgroundSize="contain"
-    backgroundColor="#fff"
-  >
+  <Slide>
+    <Heading>Api-platform/admin</Heading>
+    <iframe src="http://localhost:5173/admin" height="100%" />
     <Notes>
       API Platform Admin est un outil permettant de créer automatiquement une
       interface d'administration et complète pour toute API prenant en charge le
