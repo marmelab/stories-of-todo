9448cb6d1114ff85ff6343da84c3ea0f6e327a83
adrien guernier <adrien@marmelab.com>
2024-09-18 19:09:34 +0200

    feat: display not found message

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -19,12 +19,38 @@ export const List = () => {
   const handleDeleteTodo = (id: number) => {
     deteleTodo.mutate(id);
   };
+
   if (isPending) {
-    return <span className="loading loading-spinner text-primary"></span>;
+    return (
+      <div className="flex justify-center">
+        <span className="loading loading-spinner text-primary "></span>
+      </div>
+    );
   }
   if (error) {
     return <div>Error: {error.message}</div>;
   }
+  if (data.length === 0) {
+    return (
+      <div role="alert" className="alert alert-warning">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="h-6 w-6 shrink-0 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span>No todo found</span>
+      </div>
+    );
+  }
+
   return (
     <ul>
       {data?.map((todo) => (
